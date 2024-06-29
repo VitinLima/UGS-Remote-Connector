d39469771203fb614770361fedf1a794e2e957d9
VitinLima <aguirra.vitor@gmail.com>
2024-06-28 22:44:11 -0300

    Removed old script and changed server to keep open

--- cnc.sh
+++ cnc.sh
@@ -1,4 +1,4 @@
 mkfifo p1 p2
-nc -l 4444 >p2 <p1 &
-python cnc.py <p2 >p1
+python cnc.py <p2 >p1 &
+nc -l -k 4444 >p2 <p1
 rm -rf p1 p2
--- server.sh
+++
@@ -1,5 +0,0 @@
-#stty -F /dev/ttyUSB0 cs8 -cstopb -parenb 115200
-mkfifo p1 p2
-nc -l 4444 >p2 <p1 &
-python cnc.py <p2 >p1
-rm -rf p1 p2
