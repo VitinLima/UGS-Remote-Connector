719733b30e1f46681d2681a9193edf1d765d37a8
VitinLima <aguirra.vitor@gmail.com>
2024-06-28 22:45:31 -0300

    Fixed typo

--- cnc.py
+++ cnc.py
@@ -18,8 +18,8 @@ class InputHandler(threading.Thread):
 
 
 if __name__=="__main__":
-  #with serial.Serial('/dev/ttyACM0', 115200) as ser:
-  with serial.Serial('/dev/ttyUSB0', 115200) as ser:
+  with serial.Serial('/dev/ttyACM0', 115200) as ser:
+  # with serial.Serial('/dev/ttyUSB0', 115200) as ser:
     flag = True
     input_thread = InputHandler()
     input_thread.daemon = True
@@ -27,7 +27,7 @@ if __name__=="__main__":
     while flag:
       while ser.in_waiting>0:
         print(ser.read.decode(), end='', flush=True)
-      while input_handler.in_buffer:
+      while input_thread.in_buffer:
         line = input_thread.readline()
         if line=="^C\n":
           flag = False
