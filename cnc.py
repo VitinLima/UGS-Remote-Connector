e00f45f00994106e3458a4b2b8816e116355f1bc
VitinLima <aguirra.vitor@gmail.com>
2024-07-03 22:36:27 -0300

    test

--- cnc.py
+++ cnc.py
@@ -30,19 +30,19 @@ if __name__=="__main__":
     input_thread.daemon = True
     input_thread.start()
     finish_flag = False
-    with open("log.txt", 'w') as f:
-      while flag:
-        c = sys.stdin.read(1)
-        f.write(str(bytes(c,'utf-8')))
-        if finish_flag:
-          if c=='C':
-            flag = False
-            input_thread.running = False
-          else:
-            ser.write(bytes(c,'utf-8'))
-        elif c=='^':
-          finish_flag = True
+    # with open("log.txt", 'w') as f:
+    while flag:
+      c = sys.stdin.read(1)
+      # f.write(str(bytes(c,'utf-8')))
+      if finish_flag:
+        if c=='C':
+          flag = False
+          input_thread.running = False
         else:
           ser.write(bytes(c,'utf-8'))
+      elif c=='^':
+        finish_flag = True
+      else:
+        ser.write(bytes(c,'utf-8'))
     input_thread.running = False
     input_thread.join()
